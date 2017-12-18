9681046f356c4cde3e420e1df229d5acb3fbe53d
Kamil Lenkiewicz <kamil1006@gmail.com>
2017-12-18 15:49:06 +0100

    add a few more parser tests

--- tests_src/parserTest/parserTest.cpp
+++ tests_src/parserTest/parserTest.cpp
@@ -2,6 +2,7 @@
 #include <boost/test/unit_test.hpp>
 
 #include "parsing/TupleParser.h"
+#include "parsing/exceptions/ParseException.h"
 
 namespace
 {
@@ -14,10 +15,26 @@ namespace
     }
 }
 
+BOOST_AUTO_TEST_CASE(parser_parses_tuple_with_integer)
+{
+    auto tuple = get_parser_output("(50)");
+    BOOST_CHECK(tuple->get(0).get_int() == 50);
+}
+
+BOOST_AUTO_TEST_CASE(parser_parses_tuple_with_string)
+{
+    auto tuple = get_parser_output("(\"lol\")");
+    BOOST_CHECK(tuple->get(0).get_string() == "lol");
+}
 
 BOOST_AUTO_TEST_CASE(parser_parses_tuple_with_integer_and_string)
 {
     auto tuple = get_parser_output("(10, \"abc\")");
     BOOST_CHECK(tuple->get(0).get_int() == 10);
     BOOST_CHECK(tuple->get(1).get_string() == "abc");
+}
+
+BOOST_AUTO_TEST_CASE(parser_throws_on_missing_comma_between_elements)
+{
+    BOOST_CHECK_THROW(get_parser_output("(10 \"abc\")"), ParseException);
 }
